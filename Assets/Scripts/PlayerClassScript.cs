703e505521fcdd98131a75af0bffc5baf61d02ba
JuanFerrer <juanemilioferrer@outlook.com>
2017-03-02 13:53:12 +0000

    Changes to comply with new interface specification
    Added a couple of functions and changed others

--- Assets/Scripts/PlayerClassScript.cs
+++ Assets/Scripts/PlayerClassScript.cs
@@ -9,11 +9,15 @@ namespace Pool
     {
         private int mPlayerNo;
         private BallType mPlayerType;
+        private int mScore;
 
         public int GetPlayerNo() { return mPlayerNo; }
         public BallType GetPlayerType() { return mPlayerType; }
+        public int GetScore() { return mScore; }
 
         public void SetPlayerNo(int newVal) { mPlayerNo = newVal; }
         public void SetPlayerType(BallType newVal) { mPlayerType = newVal; }
+
+        public void SetScore(int newVal) { mScore = newVal; }
     }
 }
