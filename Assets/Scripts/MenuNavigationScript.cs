a57f47f2fdb6a7c2c1fe0ef903d298c5c207302b
JuanFerrer <juanemilioferrer@outlook.com>
2017-03-21 18:41:19 +0000

    Added menu
    Menu set up as main scene. You load other scenes from that one. Also put
    all functions from menu in one script.

---
+++ Assets/Scripts/MenuNavigationScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuNavigationScript : MonoBehaviour {
+
+    // Use this for initialization
+    public void LoadByIndex(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
+    }
+
+    public void Quit()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+}
